623de5f59b78be5b7e2c0c6ddec9f53939334de2
bhattigurjot <bhattigurjot@gmail.com>
2018-05-28 13:06:39 -0600

    Basic Vector3D class with precision control

--- include/core.h
+++ include/core.h
@@ -1,6 +1,24 @@
-#ifndef GB_CORE_H
-#define GB_CORE_H
+#ifndef CORE_H
+#define CORE_H
 
+#include "precision.h"
 
+namespace gbp 
+{
+	// Vector 3D Class
+	class Vector3D 
+	{
+	public:
+		// Default constructor
+		Vector3D(): x(0.0), y(0.0), z(0.0) {}
+		// Explicit constructor
+		Vector3D(real _x, real _y, real _z) : x(_x), y(_y), z(_z) {}
+	private:
+		real x; // x component of 3d vector
+		real y; // y component of 3d vector
+		real z; // z component of 3d vector
+	};
 
-#endif // GB_CORE_H
+}
+
+#endif // CORE_H
---
+++ include/precision.h
@@ -0,0 +1,12 @@
+#ifndef PRECISION_H
+#define PRECISION_H
+
+namespace gbp
+{
+	// For precision control
+	// single precision - float
+	// double precision - double
+	typedef float real;
+}
+
+#endif // PRECISION_H
